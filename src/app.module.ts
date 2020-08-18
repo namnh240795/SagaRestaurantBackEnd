ec48981db60bcf67ddfc1fa9a084e26c7be43568
Nguyen Hoai Nam <namnguyen@Nguyens-MacBook-Pro-3.local>
2020-08-18 14:48:31 +0700

    create models

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { OrdersController } from './orders/orders.controller';
+import { OrdersService } from './orders/orders.service';
+import { TagsController } from './tags/tags.controller';
+import { TagsService } from './tags/tags.service';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  controllers: [AppController, UsersController, OrdersController, TagsController, TasksController],
+  providers: [AppService, UsersService, OrdersService, TagsService, TasksService],
 })
 export class AppModule {}
