0c043bbf5baa38a0edec27ea6d8f33507b8c9290
Nguyen Hoai Nam <namnguyen@Nguyens-MacBook-Pro-3.local>
2020-09-13 23:14:26 +0700

    add token expired after 3600s

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { jwtConstants } from './constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '3600s' },
     }),
   ],
   controllers: [AuthController],
