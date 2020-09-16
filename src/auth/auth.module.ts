28ef1d6056f728466552cf63918268662a37a9ab
Nguyen Hoai Nam <namnguyen@Nguyens-MacBook-Pro-3.local>
2020-09-16 15:21:31 +0700

    update expiration times

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '3600s' },
     }),
   ],
   controllers: [AuthController],
